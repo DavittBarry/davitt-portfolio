72d5960c9ffe20fc12f1a225905ff737c1edfac1
Davitt Barry <davittbarry333@gmail.com>
2023-10-06 09:05:57 +0300

    Adding new CVs

--- src/partials/CVDownloadfi.tsx
+++ src/partials/CVDownloadfi.tsx
@@ -6,14 +6,14 @@ const CVDownloadfi = () => (
       <h2 className="mb-8 text-3xl font-semibold">Lataa CV:ni</h2>
       <div className="mb-8 flex flex-col lg:flex-row">
         <a
-          href="/Davitt-Barry-CV-English-2023.pdf"
+          href="/Davitt-Barry-CV-English.pdf"
           download
           className="mb-4 rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-700 lg:mb-0 lg:mr-4"
         >
           Lataa englanniksi
         </a>
         <a
-          href="/Davitt-Barry-CV-Suomeksi-2023.pdf"
+          href="/Davitt-Barry-CV-Suomeksi.pdf"
           download
           className="rounded-full bg-green-500 px-4 py-2 text-white hover:bg-green-700"
         >
